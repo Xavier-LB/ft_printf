82da5950121d7630650f54baebdab87b022df608
XavierLB <61973781+Xavier-LB@users.noreply.github.com>
2022-07-20 12:36:21 -0400

    Update for github

--- srcs/ft_putstr_len.c
+++ srcs/ft_putstr_len.c
@@ -6,7 +6,7 @@
 /*   By: xle-baux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 13:44:22 by xle-baux          #+#    #+#             */
-/*   Updated: 2021/12/06 11:35:29 by xle-baux         ###   ########.fr       */
+/*   Updated: 2021/12/06 12:37:06 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_putstr_len(char *str, int len)
 {
 	int		i;
-	char	null[6];
+	char	*null;
 
 	i = 0;
 	null = "(null)";
